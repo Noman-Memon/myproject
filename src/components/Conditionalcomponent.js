2957f8004569c38ca5d05cd4ccc564d077c9257f
ABASITJAMALBROTHERS <noman.rauf@jamalbrothers.com.pk>
2022-10-26 18:30:40 +0500

    ternary approach

--- src/components/Conditionalcomponent.js
+++ src/components/Conditionalcomponent.js
@@ -2,13 +2,25 @@ import React from 'react'
 import { useState } from 'react'
 
 export default function ConditionalComponent() {
-  const [display] = useState(false)
-  let output //use of "element variable"
+  const [display] = useState(true)
+  // let output //use of "element variable"
 
-  if (display) {
-    output = <h2>Display True</h2>
-  } else {
-    output = <h2>Display False</h2>
-  }
-  return <div>{output}</div>
+  // if (display) {
+  //   output = <h2>Display True</h2>
+  // } else {
+  //   output = <h2>Display False</h2>
+  // }
+  // return <div>{output}</div>
+
+  // ternary approach
+
+  return display ? (
+    <div>
+      <h2>Display True</h2>
+    </div>
+  ) : (
+    <div>
+      <h2>Display false</h2>
+    </div>
+  )
 }
